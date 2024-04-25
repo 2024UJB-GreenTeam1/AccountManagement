368ec27eb7011a3f9e0d6af287699822ffd45dd0
21-manic-011g <21-manic-011g@21-manic-011>
2024-04-25 16:39:24 +0900

    첫화면/로그인실패팝업

--- src/login/LoginFrame.java
+++ src/login/LoginFrame.java
@@ -13,42 +13,37 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.util.ArrayList;
 
-public class LoginFrame extends WindowAdapter {
+//extends WindowAdapter 
+public class LoginFrame extends JFrame {
 	private JFrame f;
 	private JTextField tfId, tfMsg;
 	private JPasswordField tfPwd;
-	private JButton btnL,btnI,btnP,btnS;
+	private JButton btnL, btnI, btnP, btnS;
 	private InfoDAO dao;
 	ArrayList<InfoVo> list;
 	private JPanel tab1Panel;
 
-	
 	public LoginFrame() {
-		
 
 		dao = new InfoDAO();
-		
+
 		f = new JFrame("Login");
 		f.setSize(700, 700);
 		f.getContentPane().setLayout(null);
-		
+
 		tab1Panel = new JPanel();
 		JLabel jLabel = new JLabel();
-		jLabel.setIcon(new ImageIcon(getClass().getResource("../img/logo.jpg")));//bin폴더 넘어가면 안되는듯?
+		jLabel.setIcon(new ImageIcon(getClass().getResource("../img/logo.jpg")));// bin폴더 넘어가면 안되는듯?
 		tab1Panel.add(jLabel);
 		tab1Panel.setLocation(140, 10);
 		tab1Panel.setSize(400, 400);
-		
+
 		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 		Point centerPoint = ge.getCenterPoint();
 		int leftTopX = centerPoint.x - f.getWidth() / 2;
 		int leftTopY = centerPoint.y - f.getHeight() / 2;
 		f.setLocation(leftTopX, leftTopY);
-		
-		
 
-		
-		
 		JLabel lId = new JLabel("ID");
 		lId.setLocation(170, 440);
 		lId.setSize(50, 50);
@@ -72,47 +67,47 @@ public class LoginFrame extends WindowAdapter {
 		btnL = new JButton("Login");
 		btnL.setLocation(420, 448);
 		btnL.setSize(80, 110);
-		
+
 		btnI = new JButton("ID찾기");
 		btnI.setLocation(170, 570);
 		btnI.setSize(100, 40);
-		
+
 		btnP = new JButton("PWD찾기");
 		btnP.setLocation(285, 570);
 		btnP.setSize(100, 40);
-		
+
 		btnS = new JButton("SIGN IN");
 		btnS.setLocation(400, 570);
 		btnS.setSize(100, 40);
-		
-		
-		
+
 		btnL.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.out.println(tfId.getText() + " : " + tfPwd.getText());
 				String id = tfId.getText();
-				
+
 				list = dao.list(id);
-				
-				if(list.size() != 0) {
+
+				if (list.size() != 0) {
 					for (int i = 0; i < list.size(); i++) {
 						InfoVo data = (InfoVo) list.get(i);
 						String gid = data.getId();
 						String gpwd = data.getPwd();
 
 						System.out.println(gid + " :: " + gpwd);
-						
-						if(tfId.getText().equals(gid) && tfPwd.getText().equals(gpwd)) {
-							tfMsg.setText("로그인이 성공했습니다.");		
-						}else {
-							tfMsg.setText("로그인이 실패했습니다.");
+
+						if (tfId.getText().equals(gid) && tfPwd.getText().equals(gpwd)) {
+							tfMsg.setText("로그인이 성공했습니다.");
+						} else {
+							new ResisterForm();
+//							tfMsg.setText("로그인이 실패했습니다.");
 						}
 					}
-				}else {
-					tfMsg.setText("로그인이 실패했습니다.");
+				} else {
+					new ResisterForm();
+//					tfMsg.setText("로그인이 실패했습니다.");
 				}
-				
+
 				for (int i = 0; i < list.size(); i++) {
 					InfoVo data = (InfoVo) list.get(i);
 					String gid = data.getId();
@@ -124,7 +119,6 @@ public class LoginFrame extends WindowAdapter {
 		});
 		f.getContentPane().add(tab1Panel);
 
-		
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //		f.addWindowListener(this);
 		f.add(lId);
@@ -140,13 +134,60 @@ public class LoginFrame extends WindowAdapter {
 
 		f.setVisible(true);
 	}
+
+	public class ResisterForm extends JFrame {
+		public ResisterForm() {
+			setSize(300, 200);
+			setVisible(true);
+			getContentPane().setLayout(null);
+
+			JLabel lFail = new JLabel("로그인이 실패했습니다.");
+			lFail.setLocation(40, 10);
+			lFail.setSize(200, 50);
+
+			JLabel lFail2 = new JLabel("ID혹은 PWD를 다시 확인하여 주십시오.");
+			lFail2.setLocation(40, 30);
+			lFail2.setSize(290, 50);
+
+			JButton btnE = new JButton("확인");
+			btnE.setLocation(95, 80);
+			btnE.setSize(100, 40);
+
+			btnE.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					dispose();
+				}
+			});
+
+			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+			Point centerPoint = ge.getCenterPoint();
+			int leftTopX = centerPoint.x - getWidth() / 2;
+			int leftTopY = centerPoint.y - getHeight() / 2;
+			setLocation(leftTopX, leftTopY);
+
+			this.add(lFail);
+			this.add(lFail2);
+			this.add(btnE);
+
+		}
+
+	}
+
 	
-//	public void paint(Graphics g) {//그리는 함수
-//		g.drawImage(background, 0, 0, null);//background를 그려줌
-//	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	public static void main(String[] args) {
 		new LoginFrame();
-		
+
 	}
 }
