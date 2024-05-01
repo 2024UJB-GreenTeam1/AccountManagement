65928d8e78310b4642e8d2f69abb24f9314fdfba
21-manic-004g <21-manic-004g@21-manic-004>
2024-05-01 09:31:19 +0900

    게시판 기본 커밋

---
+++ src/Board/ConnectionB.java
@@ -0,0 +1,28 @@
+package Board;  //DB 접속 클래스
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class ConnectionB {
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	public static final String DRIVER_NAME = "oracle.jdbc.OracleDriver";
+	public static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	public static final String USERID = "c##green";
+	public static final String USERPWD = "green1234";
+	
+	
+	public ConnectionB() {
+		try { 
+			Class.forName(DRIVER_NAME);
+			
+			conn = DriverManager.getConnection(URL,USERID,USERPWD);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
